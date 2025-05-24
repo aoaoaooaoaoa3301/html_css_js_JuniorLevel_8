ba9a62558c1abb42ae13faef70636f1506e4a5b3
lookki <lookkiwtf@mail.ru>
2025-05-24 20:31:08 +0300

    add functional and errors

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,14 @@
 const button = document.getElementById("button");
 
 
+var errorEmpty_day = false;
+var errorEmpty_month = false;
+var errorEmpty_year = false;
+
+var errorCorrect_day = false;
+var errorCorrect_month = false;
+var errorCorrect_year = false;
+
 button.addEventListener('click', function(){
     const output_day = document.getElementById("output-day");
     const output_year = document.getElementById("output-year");
@@ -12,49 +20,141 @@ button.addEventListener('click', function(){
     var input_month = document.getElementById("input-month");
     var input_year = document.getElementById("input-year");
 
-
-    const input_date = new Date(`${input_year.value}-${input_month.value}-${input_day.value}`);
-    const now_date = new Date();
-
-    var flag_day = false;
-    var flag_month = false;
-    var flag_year = false;
-
-    if(input_date.getFullYear() <= now_date.getFullYear()){
-        flag_year = true;
-        if(input_date.getMonth() > 0 && input_date.getMonth() <= 12){
-            flag_month = true;
-            if(input_date.getDate() > 0 && input_date.getDate() <= getDaysInMonth(input_date.getFullYear(), input_date.getMonth())){
-                flag_day = true;
-                input_containers.forEach(el => function(){
-                    el.classList.add("input-correct");
-                    el.classList.remove("input-incorrect");
-                    console.log(el.classList);
-                });
-            }
-        }
+    if (!input_day.value && !errorEmpty_day){
+        errorEmpty_day = createError(input_day, input_containers, "This field is required");
+    }
+    else if (input_day.value && errorEmpty_day){
+        console.log("bb");
+        errorEmpty_day = false;
+        input_day.parentElement.querySelector(".p-error").remove();
     }
 
-    if(flag_day && flag_month && flag_year) {
+    if (!input_month.value && !errorEmpty_month){
+        errorEmpty_month = createError(input_month, input_containers, "This field is required");
     }
-    else{
-        input_containers.forEach(el => function(){
-            console.log(el.classList);
-            el.classList.add("input-incorrect");
-            
-            el.classList.remove(".input-correct");
-            
-        });
+    else if (input_month.value && errorEmpty_month){
+        errorEmpty_month = false;
+        input_month.parentElement.querySelector(".p-error").remove();
     }
 
+    if (!input_year.value && !errorEmpty_year){
+        errorEmpty_year = createError(input_year, input_containers, "This field is required");
+    }
+    else if (input_year.value && errorEmpty_year){
+        errorEmpty_year = false;
+        input_year.parentElement.querySelector(".p-error").remove();
+    }
     
 
+    if (input_year.value && input_month.value && input_day.value){
+        const now_date = new Date();
+    
+        let flag_day = false;
+        let flag_month = false;
+        let flag_year = false;
+        
+        
+        if(input_day.value > 0 && input_day.value <= getDaysInMonth(input_year.value, input_month.value)){
+            flag_day = true;
+        }
+        if((input_year.value <= now_date.getFullYear())){
+            flag_year = true;
+        }
+        if(input_month.value > 0 && input_month.value <= 12){
+            flag_month = true;
+        }
 
-    output_day.textContent = now_date.getDate();
-    output_month.textContent = input_month.value;
-    output_year.textContent = input_year.value;
+
+        if(!(flag_day && flag_month && flag_year)) {
+            console.log("error");
+            output_day.textContent = "- -";
+            output_month.textContent = "- -";
+            output_year.textContent = "- -";
+            
+            input_containers.forEach(el =>{
+                el.classList.add("input-incorrect");
+                el.classList.remove("input-correct");
+            });
+
+            if (!flag_day && !errorCorrect_day){
+                createError(input_day, input_containers, "Must be valide day");
+                errorCorrect_day = true;
+            }
+            else if (flag_day && errorCorrect_day){
+                input_day.parentElement.querySelector(".p-error").remove();
+                errorCorrect_day = false;
+            }
+            
+            if (!flag_month && !errorCorrect_month){
+                createError(input_month, input_containers, "Must be valide month");
+                errorCorrect_month = true;
+            }
+            else if (flag_month && errorCorrect_month){
+                input_month.parentElement.querySelector(".p-error").remove();
+                errorCorrect_month = false;
+            }
+
+            if (!flag_year && !errorCorrect_year){
+                createError(input_year, input_containers, "Must be valide year");
+                errorCorrect_year = true;
+            }
+            else if (flag_year && errorCorrect_year){
+                input_year.parentElement.querySelector(".p-error").remove();
+                errorCorrect_year = false;
+            }
+        }
+
+
+        else if(flag_day && flag_month && flag_year) {
+            const input_date = new Date(`${input_year.value}-${input_month.value}-${input_day.value}`);
+
+            input_containers.forEach(el =>{
+                el.classList.add("input-correct");
+                el.classList.remove("input-incorrect");
+            });
+
+            output_day.textContent = getFullAge(input_date)[2];
+            output_month.textContent = getFullAge(input_date)[1];
+            output_year.textContent = getFullAge(input_date)[0];
+
+            document.querySelectorAll(".p-error").forEach(el =>{el.remove()});
+        }
+    }
 });
 
 function getDaysInMonth(year, month) {
   return new Date(year, month+1, 0).getDate();
+}
+
+function createError(input_n, input_containers, massage){
+    var error = document.createElement("p");
+    error.classList.add("p-error");
+    error.textContent = massage;
+
+    input_n.parentElement.appendChild(error);
+    input_containers.forEach(el =>{
+        el.classList.add("input-incorrect");
+        el.classList.remove("input-correct");
+    });
+    return true;
+}
+
+function getFullAge(date) {
+    const today = new Date();
+    const input_date = new Date(date);
+    
+    let years = today.getFullYear() - input_date.getFullYear();
+    let months = today.getMonth() - input_date.getMonth();
+    let days = today.getDate() - input_date.getDate();
+    
+    if (days < 0) {
+        months--;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+    
+    return [ years, months, days ];
 }
